48698ece47f14b3b0aa9add5ba163e0261c2a004
Maureen Njihia <maureennjihia468@gmail.com>
2022-08-15 19:46:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+const theBeatles = [
+    "John Lennon",
+    "Paul McCartney",
+    "Ringo Starr",
+    "George Harrison",
+];
+
+console.log(theBeatles);
+
+const englishBandsByCity = {
+    liverpool: "The Beatles",
+    manchester: "The Smiths",
+    coventry: "Delia Derbyshire and the BBC Radiophonic Band",
+    london: "Ziggy Stardust and the Spiders from Mars",
+};
+
+console.log(englishBandsByCity);
+
+//Array of objects
+const englishMusicByCity = {
+    manchester: [
+      {
+        bandName: "The Smiths",
+        memberNames: ["Morrissey", "Johnny", "Andy", "Mike"],
+      },
+      {
+        bandName: "Joy Division",
+        memberNames: ["Peter", "Stephen", "Bernard", "Ian"],
+      },
+    ],
+  };
